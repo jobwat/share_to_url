773bac2725e56b27a99ff4faba08e5a43d1dbdc7
Benjamin Grynblat <benjamin.grynblat@gmail.com>
2016-12-15 17:15:44 +1100

    Transparent background

--- app/src/main/java/net/fonkyfonk/shareurl/MainActivity.java
+++ app/src/main/java/net/fonkyfonk/shareurl/MainActivity.java
@@ -14,16 +14,16 @@ import android.widget.Toast;
 
 import java.net.URLEncoder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     String URL = "https://requestb.in/tiozopti";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
                 String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
                 handleSendText(sharedText); // Handle text being sent
             }
+        } else {
+            finish();
         }
     }
 
@@ -73,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
